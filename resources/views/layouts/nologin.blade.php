49fa82d42004fa3b4a53af39289e280dc29f6065
Dieison S. Borges <dieisoncomix@gmail.com>
2019-03-16 00:20:57 -0300

    logo

--- resources/views/layouts/nologin.blade.php
+++ resources/views/layouts/nologin.blade.php
@@ -7,10 +7,8 @@
 <div class="wrapper">
   <div class="col-md-2">
     <a href="/">
-        <b style="display:none;">SICARIO</b>
-        <br>
-        <img src="{{ asset('img/logo/logo-ecardume.png') }}" width="100%">
-        
+        <b style="display:none;">SICARIO</b>        
+        <img src="{{ asset('img/logo/logo-ecardume.png') }}" width="70%">        
     </a>     
   </div>
    
