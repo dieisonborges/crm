2a672b6d9d40c5a6e2651f62d58e683099ca326f
Dieison Borges <dieisoncomix@gmail.com>
2019-03-29 22:47:43 -0300

    Modificacoes Dashboard Atendimento

--- resources/views/atendimento/dashboard.blade.php
+++ resources/views/atendimento/dashboard.blade.php
@@ -94,6 +94,162 @@
    
       <!-- Info boxes -->
       <div class="row">
+
+
+
+
+
+
+        @foreach($tickets as $ticket)
+
+        <!-- Pessima pratica - melhorar -->
+        @//php
+            //unset($prontuario_tmp);
+            //$prontuario_tmp[] = $prontuarios[$ticket->id];
+        @//endphp
+
+
+       <div class="col-md-4">
+          <!-- DIRECT CHAT PRIMARY -->
+          <!--
+          0   =>  "Crítico - Emergência (resolver imediatamente)",
+          1   =>  "Alto - Urgência (resolver o mais rápido possível)",
+          2   =>  "Médio - Intermediária (avaliar situação)",
+          3   =>  "Baixo - Rotineiro ou Planejado",
+          4   =>  "Nenhum",
+          -->
+          @switch($ticket->rotulo)
+              @case(0)
+                  <div class="box box-danger direct-chat direct-chat-primary collapsed-box">
+              @break
+              @case(1)
+                  <div class="box box-warning direct-chat direct-chat-primary collapsed-box">
+              @break
+              @case(2)
+                  <div class="box box-info direct-chat direct-chat-primary collapsed-box">
+              @break
+              @case(3)
+                  <div class="box box-default direct-chat direct-chat-primary collapsed-box">
+              @break
+              @case(4)
+                  <div class="box box-primary direct-chat direct-chat-primary collapsed-box">                  
+              @break
+          @endswitch         
+
+
+            <div class="box-header with-border">
+              <h3 class="box-title">
+                <a href="{{url('atendimentos/'.$setor->name.'/'.$ticket->id.'/show')}}" class="text-black" style="font-size: 25px;">
+                  {{ str_limit($ticket->titulo, $limit = 20, $end = '...') }}
+                </a></h3><br>
+              <a href="{{url('atendimentos/'.$setor->name.'/'.$ticket->id.'/show')}}">Ticket {{$ticket->id}} Nº <b>{{$ticket->protocolo}}</b></a><br>
+              <small>Aberto há <b>{{floor((strtotime(date('Y-m-d')) - strtotime(date('Y-m-d', strtotime($ticket->created_at)))) / (60 * 60 * 24))}} dia(s)</b></small><br>
+
+              <!--
+              @//foreach($prontuario_tmp as $prontuario)
+                @//if($prontuario['descricao'])
+
+                  <small>Última ação há <b>{//{//floor((strtotime(date('Y-m-d')) - strtotime(date('Y-m-d', strtotime($prontuario['created_at'])))) / (60 * 60 * 24))}} dia(s)</b></small><br>
+
+                @//else
+                  <small>Nenhuma ação.</small><br>
+                @//endif
+              @//endforeach
+              -->
+              <!--
+              0   =>  "Crítico - Emergência (resolver imediatamente)",
+              1   =>  "Alto - Urgência (resolver o mais rápido possível)",
+              2   =>  "Médio - Intermediária (avaliar situação)",
+              3   =>  "Baixo - Rotineiro ou Planejado",
+              4   =>  "Nenhum",
+              -->
+              <!--
+              <div class="box-tools pull-right">
+                @//switch($ticket->rotulo)
+                    @//case(0)
+                        <span data-toggle="tooltip" title="Crítico" class="badge bg-red">Crítico</span>
+                    @//break
+                    @//case(1)
+                        <span data-toggle="tooltip" title="Alto" class="badge bg-yellow">Alto</span>
+                    @//break
+                    @//case(2)
+                        <span data-toggle="tooltip" title="Médio" class="badge bg-purple">Médio</span>
+                    @//break
+                    @//case(3)
+                        <span data-toggle="tooltip" title="Baixo" class="badge bg-navy">Baixo</span>
+                    @//break
+                    @//case(4)
+                        <span data-toggle="tooltip" title="Nenhum" class="badge bg-blue">Nenhum</span>
+                    @//break
+                @//endswitch
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
+              </div>
+            -->
+
+
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <!-- Conversations are loaded here -->
+              <div class="direct-chat-messages">
+                <!-- Message. Default to the left -->
+
+                
+                @//foreach($prontuario_tmp as $prontuario)
+                  @//if($prontuario['descricao'])
+                    <div class="direct-chat-msg">
+                    <div class="direct-chat-info clearfix">
+                      <!--<span class="direct-chat-name pull-left">{//{//$prontuario['user_id']}}</span>-->
+                      <span class="direct-chat-timestamp pull-right">{//{//date('d/m/Y H:i:s', strtotime($prontuario['created_at']))}}</span>
+                    </div>
+                    <!-- /.direct-chat-info -->
+                    <img class="direct-chat-img" src="{{ asset('img/default-user-image.png') }}" alt="message user image">
+                    <!-- /.direct-chat-img -->
+                    <div class="direct-chat-text">
+                      {//!! html_entity_decode($prontuario['descricao']) !!}
+                    </div>
+                    <!-- /.direct-chat-text -->
+                  </div>
+                  <!-- /.direct-chat-msg -->
+                @//else
+
+
+                @//endif
+
+
+                @//endforeach
+
+
+              </div>
+              <!--/.direct-chat-messages-->
+
+           
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+              <a href="{//{//url('tecnicos/'.$setor->name.'/'.$ticket->id.'/show')}}" class="btn btn-success"><i class="fa fa-plus"></i> Mais Informações</a>
+              <a href="{//{//URL::to('tecnicos')}}/{//{//$setor->name}}/{//{//$ticket->id}}/encerrar"  style="float: right;" class="btn btn-danger btn-md"><i class="fa fa-times-circle"></i> Encerrar Ticket</a>            
+            </div>
+            <!-- /.box-footer-->
+          </div>
+          <!--/.direct-chat -->
+        </div>
+        <!-- /.col -->        
+
+        @endforeach 
+
+
+
+
+
+
+
+
+
+
+
+
         
 
         @foreach($tickets as $ticket)
