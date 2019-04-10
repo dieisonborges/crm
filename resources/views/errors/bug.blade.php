4e7c256f50890f69cdcccb389501526bc5a095ba
Dieison Borges <dieisoncomix@gmail.com>
2019-04-10 15:09:44 -0300

    Erro

---
+++ resources/views/errors/bug.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.login')
+@section('content')
+<div class="login-box">
+  
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+
+    <a href="/">
+        <b style="display:none;">e-Cardume</b>
+        <img src="{{ asset('img/logo/logo-ecardume.png') }}" width="100%">
+    </a>
+
+    <hr>
+
+    @include('layouts.error')
+
+    
+    <div class="alert alert-warning alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-ban"></i> Erro: 500</h4>
+        Todo mundo erra um dia, e hoje parece que foram nossos servidores... :(
+
+        <br>
+        <small>Whoops, looks like something went wrong</small>
+        <small>Opa, parece que algo deu errado</small>
+    </div>
+
+    <a class="btn btn-primary" href="javascript:history.go(-1)">Voltar</a>
+
+       
+
+
+   
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+@endsection
