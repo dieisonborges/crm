7af8600bfde032b428cd1b6027b3eb07b2c93c96
Dieison Borges <dieisoncomix@gmail.com>
2019-08-11 20:44:17 -0300

    Loja do Franqueado

--- app/Http/Controllers/LojaFranqueadoController.php
+++ app/Http/Controllers/LojaFranqueadoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 //Log
 use App\Http\Controllers\Log;
@@ -41,7 +42,42 @@ class LojaFranqueadoController extends Controller
             $this->log("loja_franqueado.index");
             //--------------------------------------------------------------------------------------
 
-            return view('loja_franqueado.index');
+                    return Redirect::to("https://ecardu.me/tglebeaw3dhtc4e/");
+
+                    /*
+
+                    $username="teste";
+                    $password="teste@123";
+                    $url="https://ecardu.me/tglebeaw3dhtc4e/";
+                    $cookie="/var/www/html/cookie.txt";
+
+                    $postdata = "log=". urlencode($username) ."&pwd=". urlencode($password)."&wp-submit=Log%20In&redirect_to=". $url ."wp-admin/&testcookie=1";
+                    $ch = curl_init();
+                    curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie);  
+                    curl_setopt ($ch, CURLOPT_COOKIESESSION, 'TRUE');
+                    curl_setopt ($ch, CURLOPT_URL, $url . "wp-login.php");
+                    curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+                    curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+                    curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
+                    curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+                    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie);
+                    curl_setopt ($ch, CURLOPT_REFERER, $url . "wp-admin/");
+                    curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata);
+                    curl_setopt ($ch, CURLOPT_POST, 1);
+                    $result = curl_exec ($ch);
+                    curl_close($ch);
+
+                    echo $result;
+
+                    exit;
+                    die();
+
+                    */               
+                    
+                    
+
+                    //return view('loja_franqueado.index');
         }
         else{
             return view('errors.403');
